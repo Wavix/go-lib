1198088bf58738cbd4b39384f667618fae7104dd
Mike K <mike.k@wavix.com>
2024-08-08 15:58:31 +0500

    Add system stats to metrics

--- go.mod
+++ go.mod
@@ -5,12 +5,17 @@ go 1.21.5
 require github.com/wagslane/go-rabbitmq v0.14.2
 
 require (
+	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	golang.org/x/sys v0.18.0 // indirect
+	github.com/tklauser/go-sysconf v0.3.14 // indirect
+	github.com/tklauser/numcpus v0.8.0 // indirect
+	github.com/yusufpapurcu/wmi v1.2.4 // indirect
+	golang.org/x/sys v0.19.0 // indirect
 )
 
 require (
 	github.com/fatih/color v1.17.0
 	github.com/rabbitmq/amqp091-go v1.10.0 // indirect
+	github.com/shirou/gopsutil v3.21.11+incompatible
 )
--- metrics/metrics.go
+++ metrics/metrics.go
@@ -3,8 +3,15 @@ package metrics
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"log"
+	"os"
+	"runtime"
+	"strconv"
+	"time"
 
+	"github.com/shirou/gopsutil/mem"
+	"github.com/shirou/gopsutil/process"
 	"github.com/wagslane/go-rabbitmq"
 	"github.com/wavix/go-lib/amqp"
 )
@@ -39,6 +46,8 @@ func Init(amqpDsn string, service string) (*MetricInstance, error) {
 		Connection: publisher,
 		Service:    service,
 	}
+
+	go instance.startSystemStats()
 
 	return instance, nil
 }
@@ -78,4 +87,55 @@ func (m *MetricInstance) SetGauge(key string, value int) {
 	if err != nil {
 		log.Println("Failed to publish gauge")
 	}
+}
+
+func (m *MetricInstance) startSystemStats() {
+	if m == nil || m.Connection == nil {
+		return
+	}
+
+	for {
+		cpu, mem, err := getCurrentProcessUsage()
+		if err != nil {
+			log.Println("Error in application cpu/mem stats: ", err)
+			time.Sleep(10 * time.Second)
+		}
+
+		m.SetGauge("cpu", int(cpu))
+		m.SetGauge("mem", int(mem))
+
+		time.Sleep(10 * time.Second)
+	}
+
+}
+
+func getCurrentProcessUsage() (int64, int64, error) {
+	pid := os.Getpid()
+	process, err := process.NewProcess(int32(pid))
+	if err != nil {
+		return 0, 0, err
+	}
+
+	cpuPercent, err := process.CPUPercent()
+	if err != nil {
+		return 0, 0, err
+	}
+
+	var memStats runtime.MemStats
+	runtime.ReadMemStats(&memStats)
+
+	vmem, err := mem.VirtualMemory()
+	if err != nil {
+		return 0, 0, err
+	}
+
+	memPercent := float32(memStats.Alloc) / float32(vmem.Total) * 100
+
+	return formatFloat(float32(cpuPercent)), formatFloat(memPercent), nil
+}
+
+func formatFloat(num float32) int64 {
+	formated := fmt.Sprintf("%.2f", num)
+	result, _ := strconv.ParseFloat(formated, 32)
+	return int64(result)
 }
